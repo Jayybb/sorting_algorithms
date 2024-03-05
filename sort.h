651ed8b0941ece47b7c723ab4d13fbef8f33b67d
Abexholy <abexholy123@gmail.com>
2024-03-05 04:32:38 -0800

    code

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_s - The doubly linked list node
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
@@ -17,19 +17,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void _xsort(int *b, int size, int n);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bitonic_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void cocktail_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void _qsort(int *m, int low, int high, int size);
 void shell_sort(int *array, size_t size);
-void _xsort(int *b, int size, int n);
-void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
+void _qsort(int *m, int low, int high, int size);
 #endif /** SORT_H_ **/
